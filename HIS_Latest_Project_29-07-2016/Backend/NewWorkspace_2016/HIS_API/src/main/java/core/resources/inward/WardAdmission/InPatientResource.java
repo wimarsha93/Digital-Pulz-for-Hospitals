14f28cd6d0cbbd219a596dd4d5907ee69f6af153
PEROOT\VMADHWI <wimamadubashana@gmail.com>
2017-04-06 11:34:06 +0530

    problem solved

--- HIS_Latest_Project_29-07-2016/Backend/NewWorkspace_2016/HIS_API/src/main/java/core/resources/inward/WardAdmission/InPatientResource.java
+++
@@ -1,5 +0,0 @@
-package core.resources.inward.WardAdmission;
-
-public class InPatientResource {
-
-}
--- HIS_Latest_Project_29-07-2016/Backend/NewWorkspace_2016/HIS_API/src/main/java/lib/driver/inward/driver_class/WardAdmission/InPatientDBDriver.java
+++
@@ -1,5 +0,0 @@
-package lib.driver.inward.driver_class.WardAdmission;
-
-public class InPatientDBDriver {
-
-}
